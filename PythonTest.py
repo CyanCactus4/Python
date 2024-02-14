370aa08216802b2803142fb00e54126210fa8631
Евгений <depcoffmel@gmail.com>
2024-02-15 00:23:31 +0300

    Первый программный код

--- PythonTest.py
+++ PythonTest.py
@@ -1,2 +1,11 @@
-par = 5
-print(par)
+from math import *
+par = int(input("Введите положительно число:\t"))
+
+assert(par >= 0)
+
+line = f"Делители числа {par}:\n"
+print(line)
+for i in range(1, ceil(sqrt(par))+1):
+    if par % i == 0:
+        print(f"{i}\n")
+print(f"{par}\n")
