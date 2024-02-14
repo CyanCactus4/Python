4b33ec1a3d62ce5e506b780a0f0b10df931878b1
Евгений <depcoffmel@gmail.com>
2024-02-15 00:48:15 +0300

    Добавил C#

---
+++ Common.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+using static System.Math;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите число:");
+            int par = int.Parse(ReadLine());
+            WriteLine($"Все простые от 2 до {par} включительно:");
+            for (int i = 2; i <= par; i++)
+            {
+                bool Flag = true;
+                for (int j = 2; j <= Floor(Sqrt(i)); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        Flag = false;
+                        break;
+                    }
+                }
+                if (Flag)
+                    WriteLine(i);
+            }
+        }
+    }
+}
